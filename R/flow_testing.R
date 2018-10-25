e3d9724b2c10712bb7f7f8d7c132d3faf4dd0060
Krystian Igras <krystian8207@gmail.com>
2018-10-26 01:17:26 +0200

    Add some comparisons to gam.

--- R/flow_testing.R
+++ R/flow_testing.R
@@ -11,7 +11,15 @@ boston.rf <- randomForest(cmedv ~ lstat + ptratio + age, data = boston)
 model <- xp_gam(
   cmedv ~ xs(lstat, spline_opts = list(k = 6), method_opts = list(type = "pdp", grid.resolution = 60)) +
           xs(ptratio, spline_opts = list(k = 4), method_opts = list(type = "pdp", grid.resolution = 40)) +
-          xs(age, spline_opts = list(k = 2), method_opts = list(type = "pdp", grid.resolution = 20)),
+          xs(age, spline_opts = list(k = 8), method_opts = list(type = "pdp", grid.resolution = 120)),
   blackbox = boston.rf,
   data = boston
 )
+
+summary(model)
+summary(gam::gam(cmedv ~ lstat + ptratio + age, data = boston))
+
+var <- "age"
+pred_var <- pretty(range(boston[[var]]), 100)
+plot(model[["xs_env"]][[var]][["blackbox_response_obj"]])
+lines(pred_var, predict(model[["xs_env"]][[var]][["blackbox_response_approx"]], newdata = data.frame(pred_var = pred_var)))
